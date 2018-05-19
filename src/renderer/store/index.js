bf3bfd5f442ace70e1f2683bf98a5af89846c5b2
Forrest Wilson <forrest.wilson@me.com>
2018-05-19 18:58:07 +1200

    Using electron-store to save data in external JSON

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import createPersistedState from 'vuex-persistedstate'
-
 import modules from './modules'
+
+import createPersistedState from 'vuex-persistedstate'
+import ElectronStore from 'electron-store'
+const store = new ElectronStore()
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules,
   strict: process.env.NODE_ENV !== 'production',
-  plugins: [createPersistedState()]
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => store.get(key),
+        setItem: (key, value) => store.set(key, value),
+        removeItem: key => store.delete(key)
+      }
+    })
+  ]
 })
